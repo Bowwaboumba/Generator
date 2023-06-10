5dc01023278157fd0910e935c738cb32166a5ad3
Rafael <122923722+Bowwaboumba@users.noreply.github.com>
2023-06-10 22:47:40 +0200

    Add files via upload
    generates a pseudo-random number and a pseudo-random caracters sequence.

---
+++ generator.py
@@ -0,0 +1,9 @@
+import string
+import random
+begin = input("GENERATOR is a random numbers generator and sequence of letters. Change randint to change it's capacity; Press Enter. ")
+print(random.randint(1, 100))
+print(string.ascii_letters)
+
+input('Appuyez sur entrée ...')
+
+n = (random.randint(1, 100))
